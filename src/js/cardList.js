2c505f9d133e9bbe258d012226a7b3e882e4b0e4
Isabela Barižon <ibarizon@hattrick.local>
2021-11-17 13:59:34 +0100

    implementing checkbox filters

--- src/js/cardList.js
+++ src/js/cardList.js
@@ -2,11 +2,16 @@ function cardList(data) {
     this.data = data;
     this.sortBy = 'age';
     this.sortOrder = 'asc';
+    // this.lessThan6Sorted = 'sort-less-6';
+    // this.lessThan12Sorted = 'sort-less-12';
+    // this.colorBlackSorted = 'sort-black-color';
     this.visibleItems = 20;
     this.updateFiltersEvent = new Event('filtersUpdated');
 
+
     this.init();
     this.bind();
+    this.search1();
 
 }
 
@@ -18,6 +23,25 @@ cardList.prototype.init = function() {
 cardList.prototype.bind = function() {
     let sortByBtns = document.getElementsByName('sort-by');
     let sortOrderBtns = document.getElementsByName("sort-order");
+    let sortLessSixBtn = document.getElementById("less-than-six");
+    let sortLessTwelveBtn = document.getElementById("less-than-twelve");
+    let sortColorCatsBtn = document.getElementById("filter-by-color");
+
+    sortLessSixBtn.addEventListener('change', (e) => {
+        this.lessThan6Sorted = e.target.value;
+        document.dispatchEvent(this.updateFiltersEvent)
+
+    })
+
+    sortLessTwelveBtn.addEventListener('change', (e) => {
+        this.lessThan12Sorted = e.target.value;
+        document.dispatchEvent(this.updateFiltersEvent)
+    })
+
+    sortColorCatsBtn.addEventListener('change', (e) => {
+        this.colorBlackSorted = e.target.value;
+        document.dispatchEvent(this.updateFiltersEvent)
+    })
 
     sortByBtns.forEach(el => {
         el.addEventListener('change', () => {
@@ -37,9 +61,11 @@ cardList.prototype.bind = function() {
         this.sortCards();
         this.renderCards();
     })
-
+    this.onClickAdoptBtn();
+    this.closeAdoptBtn();
     this.onClickLoadMoreBtn();
-    // this.onClickAdoptBtn();
+
+
 }
 
 cardList.prototype.sortItems = function() {
@@ -51,6 +77,54 @@ cardList.prototype.sortItems = function() {
 cardList.prototype.sortCards = function() {
     this.sortByOrder();
     this.sortByValue();
+    this.sortByLessThan6();
+    this.sortByLessThan12();
+    this.sortByBlackColor();
+}
+
+cardList.prototype.sortByBlackColor = function() {
+    let sortedByColor;
+    if (this.colorBlackSorted === 'sort-black-color') {
+        sortedByColor = this.data.filter((item) => {
+            return item.color === "black";
+        });
+        this.data = sortedByColor;
+    }
+    let isChecked = document.getElementById("filter-by-color").checked;
+
+    if (isChecked) {
+        console.log("Input is checked");
+    } else {
+        console.log("Input is off")
+        this.renderCards();
+    }
+
+}
+
+cardList.prototype.sortByLessThan6 = function() {
+    let sortedLess6;
+    if (this.lessThan6Sorted === 'sort-less-6') {
+        sortedLess6 = this.data.filter((item) => {
+            return item.age < 6;
+        });
+        this.data = sortedLess6;
+    }
+    this.sortByOrder();
+    this.sortByValue();
+    this.renderCards();
+}
+
+cardList.prototype.sortByLessThan12 = function() {
+    let sortedLess12;
+    if (this.lessThan12Sorted === 'sort-less-12') {
+        sortedLess12 = this.data.filter((item) => {
+            return item.age < 12;
+        });
+        this.data = sortedLess12;
+    }
+    this.sortByOrder();
+    this.sortByValue();
+    this.renderCards();
 }
 
 cardList.prototype.sortByOrder = function() {
@@ -114,10 +188,12 @@ cardList.prototype.renderCards = function() {
                         <div class="card-color"> Color: ${element.color} </div>  
                     </div>
                 </div>
-                <button type="button" class="card-btn"> Adopt me</button>
+                <button type="button" class="card-btn" value="${element.id}"> Adopt me</button>
             </div>
-        </div>`;
+        </div>`
     });
+
+
 
     document.getElementById('cards-wrapper').innerHTML = htmlOutput;
 }
@@ -134,19 +210,71 @@ cardList.prototype.onClickLoadMoreBtn = function() {
         this.renderCards();
     })
 }
+cardList.prototype.search1 = function() {
 
+    const searchWrapper = document.querySelector(".search-input");
+    const inputBox = searchWrapper.querySelector("input");
+    let data = this.data;
 
-cardList.prototype.onClickAdoptBtn = function() {
-    let adoptCatBtn = document.getElementsByClassName("card-btn");
-    let adoptCatCard = document.getElementsByClassName("card");
-    adoptCatBtn.addEventListener("click", () => {
-        adoptCatBtn.remove() + adoptCatCard.remove();
-    })
-
-    this.renderCards();
+    inputBox.addEventListener('keyup', (e) => {
+        let found = this.data.filter(el => el.name.toLowerCase().includes(inputBox.value.toLowerCase()))
+        this.data = found;
+        if (inputBox.value === "") {
+            this.data = data;
+        }
+        this.renderCards();
+    });
 }
 
-console.log(document.getElementsByClassName("card-btn"))
+cardList.prototype.onClickAdoptBtn = function() {
+    let cardButtonModal = document.querySelectorAll(".card-btn");
+    cardButtonModal.forEach(el => {
+        el.addEventListener('click',
+            function() {
+                let modal = document.querySelectorAll(".bg-modal");
+                modal.forEach(el => {
+                    el.style.display = "flex";
+                })
+            });
+    });
 
+}
 
+cardList.prototype.closeAdoptBtn = function() {
+        let cardButtonModal = document.querySelectorAll(".close");
+        cardButtonModal.forEach(el => {
+            el.addEventListener('click',
+                function() {
+                    let modal = document.querySelectorAll(".bg-modal");
+                    modal.forEach(el => {
+                        el.style.display = "none";
+                    })
+                });
+
+        });
+
+    }
+    // cardList.prototype.closeAdoptBtn = function() {
+    //     document.getElementsByClassName('close').addEventListener('click',
+    //         function() {
+    //             document.getElementsByClassName('bg-modal').style.display
+    //         })
+
+// }
+
+// cardList.prototype.onClickAdoptBtn = function() {
+//     let adoptBtn = document.getElementsByClassName("card-btn");
+//     for (let i = 0; i < adoptBtn.length; i++) {
+//         adoptBtn[i].addEventListener('click', function(e) {
+//             console.log(e);
+//             alert(document.getElementsByClassName("modal"))
+//             console.log(adoptBtn)
+//         })
+//     }
+// }
+
+// sortOrderBtns.forEach(el => {
+//     el.addEventListener('change', () => {
+//         this.sortOrder = el.value;
+//         document.dispatchEvent(this.updateFiltersEvent)
 export default cardList;
